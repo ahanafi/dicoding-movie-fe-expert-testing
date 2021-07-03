e56bb9b604d6890260eb91d97ec97cfe74774bb9
Ahmad Hanafi <Contaminated007@gmail.com>
2021-07-03 09:03:40 +0700

    Jangan Bertele-tele

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -2,40 +2,80 @@ import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movie
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
 
 describe('Searching movies', () => {
-  beforeEach(() => {
-    document.body.innerHTML = `
-      <div id="movie-search-container">
-        <input id="query" type="text">
-        <div class="movie-result-container">
-            <ul class="movies">
-            </ul>
-        </div>
-      </div>`;
-  });
+  let presenter;
 
-  it('should be able to capture the query typed by the user', () => {
+  const searchMovies = (query) => {
+    const queryElement = document.getElementById('query');
+    queryElement.value = query;
+    queryElement.dispatchEvent(new Event('change'));
+  };
+
+  const setMovieSearchContainer = () => {
+    document.body.innerHTML = `
+        <div id="movie-search-container">
+            <input id="query" type="text">
+            <div class="movie-result-container">
+                <ul class="movies">
+                </ul>
+            </div>
+        </div>
+        `;
+  };
+
+  const constructPresenter = () => {
     spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({
+    presenter = new FavoriteMovieSearchPresenter({
       favoriteMovies: FavoriteMovieIdb,
     });
+  };
 
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'film a';
-    queryElement.dispatchEvent(new Event('change'));
+  beforeEach(() => {
+    setMovieSearchContainer();
+    constructPresenter();
+  });
+
+  // Testing 1 : Ambil query yang diketik oleh user
+  it('should be able to capture the query typed by the user', () => {
+    searchMovies('film a');
 
     expect(presenter.latestQuery)
       .toEqual('film a');
   });
 
+  // Testing 2 : Lakukan pencarian berdasarkan query user
   it('should ask the model to search for liked movies', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'film a';
-    queryElement.dispatchEvent(new Event('change'));
+    searchMovies('film a');
 
     expect(FavoriteMovieIdb.searchMovies)
       .toHaveBeenCalledWith('film a');
+  });
+
+  // Testing 3 : Tampilkan hasil pencarian
+  it('should show the found movies', () => {
+    presenter._showFoundMovies([
+      { id: 1, title: 'Film Satu' },
+      { id: 2, title: 'Film Dua' },
+    ]);
+
+    // kemudian tes film di atas ditampilkan
+    const foundMovies = document.querySelectorAll('.movie');
+
+    expect(foundMovies.length).toEqual(2);
+  });
+
+  // Testing 4 : Pencarian berdasarkan title
+  it('should show the title of the found movies', () => {
+    presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
+    expect(document.querySelectorAll('.movie__title').item(0).textContent)
+      .toEqual('Satu');
+
+    presenter._showFoundMovies([
+      { id: 1, title: 'Satu' },
+      { id: 2, title: 'Dua' },
+    ]);
+
+    const movieTitles = document.querySelectorAll('.movie__title');
+    expect(movieTitles.item(0).textContent).toEqual('Satu');
+    expect(movieTitles.item(1).textContent).toEqual('Dua');
   });
 });
